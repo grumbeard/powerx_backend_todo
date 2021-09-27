2e049a06aff40d7f5ade768778127410cb8cf0f5
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-27 16:56:39 +0800

    Update DB Schema to include Timestamps for Create, Update Delete

--- src/db/index.js
+++ src/db/index.js
@@ -14,7 +14,10 @@ module.exports = () => {
       CREATE TABLE IF NOT EXISTS TodoList (
         id SERIAL PRIMARY KEY,
         title VARCHAR(100) NOT NULL,
-        todos VARCHAR(100) ARRAY
+        todos VARCHAR(100) ARRAY,
+        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now() ,
+        deleted_at TIMESTAMP WITH TIME ZONE
       )
     `);
   };
