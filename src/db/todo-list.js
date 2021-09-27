1ae68b2f4fdca33da503759aabd5b2dddd92b946
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-27 15:45:24 +0800

    Add Endpoint: POST /todo (Create TodoList)

--- src/db/todo-list.js
+++ src/db/todo-list.js
@@ -8,5 +8,10 @@ module.exports = (pool) => {
     return res.rows.map(row => new TodoList(row));
   }
   
+  db.insertTodoList = async ({title, todos}) => {
+    const res = await pool.query('INSERT INTO TodoList (title, todos) VALUES ($1,$2) RETURNING *', [title, todos]);
+    return res.rowCount ? new TodoList(res.rows[0]) : null;
+  }
+  
   return db;
 }
--- src/routes/todo-list.js
+++ src/routes/todo-list.js
@@ -6,5 +6,11 @@ module.exports = (db) => {
     res.status(200).send(todoLists);
   })
   
+  router.post('/', async (req, res, next) => {
+    const { title, todos } = req.body;
+    const todoList = await db.insertTodoList({title, todos});
+    res.status(201).send(todoList);
+  })
+  
   return router;
 }
