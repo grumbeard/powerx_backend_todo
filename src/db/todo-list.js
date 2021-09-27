a66b9e27998a7de2122b983511d445b81d2c6270
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-27 20:16:14 +0800

    Enhancement: CRU TodoList with Optional Todos (Items)

--- src/db/todo-list.js
+++ src/db/todo-list.js
@@ -1,33 +1,61 @@
 const { TodoList } = require('../models/todo-list');
+const { Item } = require('../models/item');
 
 module.exports = (pool) => {
-  const db = {};
+  const db = {
+    ...require('./item')(pool)
+  };
   
   db.findAllTodoLists = async () => {
     const res = await pool.query('SELECT * FROM TodoList');
-    return res.rows.map(row => new TodoList(row));
+    const todoLists = res.rows.map(async row => {
+      const items = await pool.query('SELECT * FROM Item WHERE todoListId = $1', [row.id]);
+      return new TodoList({...row, todos: items.rows.map(item => new Item(item))})
+    });
+    return await Promise.all(todoLists);
   };
   
   db.insertTodoList = async ({title, todos}) => {
-    const res = await pool.query('INSERT INTO TodoList (title, todos) VALUES ($1,$2) RETURNING *', [title, todos]);
-    return res.rowCount ? new TodoList(res.rows[0]) : null;
+    const res = await pool.query('INSERT INTO TodoList (title) VALUES ($1) RETURNING *', [title]);
+    let itemsData = []
+    
+    if (todos) {
+      const items = todos.map(todo => db.insertItem({description: todo, todoListId: res.rows[0].id}));
+      
+      itemsData = await Promise.all(items);
+    }
+    return res.rowCount ? new TodoList({...res.rows[0], todos: itemsData.map(item => new Item(item))}) : null;
   };
   
   db.findTodoList = async (id) => {
     const res = await pool.query('SELECT * FROM TodoList WHERE id = $1', [id]);
-    return res.rowCount ? new TodoList(res.rows[0]) : null;
+    
+    if (res.rowCount > 0) {
+      const items = await pool.query('SELECT * FROM Item WHERE todoListId = $1', [id]);
+      return new TodoList({...res.rows[0], todos: items.rows.map(item => new Item(item))})
+    }
+    return null;
   };
   
-  db.updateTodoList = async ({id, title, todos}) => {    
+  db.updateTodoList = async ({id, title, todos}) => { 
     const res = await pool.query(`
     UPDATE TodoList 
     SET 
       title = CASE WHEN $2::VARCHAR IS NOT NULL THEN $2 ELSE title END,
-      todos = CASE WHEN $3::VARCHAR[] IS NOT NULL THEN $3 ELSE todos END,
       updated_at = now()
     WHERE id = $1 RETURNING *
-    `, [id, title, todos]);
-    return res.rowCount ? new TodoList(res.rows[0]) : null;
+    `, [id, title]);
+    
+    if (res.rowCount > 0) {
+      let itemsData = []
+      if (todos) {
+        const items = todos.map(async (todo) => db.insertItem({description: todo, todoListId: id}));
+        
+        itemsData = await Promise.all(items);
+      }
+      return new TodoList({...res.rows[0], todos: itemsData.map(item => new Item(item))})
+    } 
+    return null;
   }
   
   db.deleteTodoList = async (id) => {
