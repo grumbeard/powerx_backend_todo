8ad8d660129999cb054f5e8f55580ea842890a84
Kaiying Fu <kaiying.fu@gmail.com>
2021-10-18 23:38:49 +0800

    Enable CORS (TODO: Add Options method for OpenAPI Specs)

--- src/app.js
+++ src/app.js
@@ -18,6 +18,15 @@ module.exports = (router) => {
     })
   );
   
+  app.use(function(req, res, next) {
+    const allowedOrigins = ['http://grumbeard-powerx-todo.herokuapp.com', 'http://localhost:3000', 'http://localhost:3030'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
+  
   app.use(router);
   
   app.use((error, req, res, next) => {
